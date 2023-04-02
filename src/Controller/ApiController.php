29893b136f09432019b6427ddb1b14e5814f952c
Marian4gc <mariangarcia1724@gmail.com>
2023-04-02 13:09:28 +0200

    correcciones

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -1,27 +1,50 @@
 <?php
+// src/Controller/ApiController.php
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
-/**
- * @Route("/api")
- */
-class ApiController
+class ApiController extends AbstractController
 {
+    private $logger;
+    private $entityManager;
+
+    public function __construct(LoggerInterface $logger, EntityManagerInterface $entityManager)
+    {
+        $this->logger = $logger;
+        $this->entityManager = $entityManager;
+    }
+
     /**
-     * @Route("/birds", methods={"POST, GET"})
+     * @Route("/api/birds", name="api_birds", methods={"POST"})
      */
     public function postBirds(Request $request): Response
     {
         $data = json_decode($request->getContent(), true);
 
-        // Procesa los datos como sea necesario
-        $selectedBirdsNames = $data['selectedBirdsNames'];
+        // verificar si los datos recibidos son válidos
+        if (!isset($data['selectedBirdsData']) || !is_array($data['selectedBirdsData'])) {
+            return new JsonResponse(['error' => 'Los datos recibidos no son válidos.'], 400);
+        }
+        
+        $selectedBirdsData = $data['selectedBirdsData'];
 
-        // Devuelve los datos recibidos en la respuesta
-        return new JsonResponse($selectedBirdsNames);
+        // aquí puedes hacer lo que necesites con los datos recibidos
+        // por ejemplo, guardarlos en la base de datos
+
+        // devolver una respuesta exitosa
+        $response = new JsonResponse(['message' => 'Datos recibidos correctamente.']);
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type');
+
+        return $response;
     }
 }
