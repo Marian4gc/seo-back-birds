cb97966aaf48d5014a07e46cb8b6244c548263ca
Marian4gc <mariangarcia1724@gmail.com>
2023-04-02 13:12:30 +0200

    otra forma comentada

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -48,3 +48,68 @@ class ApiController extends AbstractController
         return $response;
     }
 }
+
+
+// <?php
+
+
+// namespace App\Controller;
+
+
+// use Symfony\Component\HttpFoundation\Request;
+// use Symfony\Component\HttpFoundation\Response;
+// use Symfony\Component\HttpFoundation\JsonResponse;
+// use Symfony\Component\Routing\Annotation\Route;
+// use Psr\Log\LoggerInterface;
+
+
+// /**
+//  * @Route("/api")
+//  */
+// class ApiController
+// {
+//     private $logger;
+
+
+//     public function __construct(LoggerInterface $logger)
+//     {
+//         $this->logger = $logger;
+//     }
+
+
+//     /**
+//      * @Route("/birds", methods={"POST"})
+//      */
+//     public function postBirds(Request $request): Response
+// {
+//     $data = json_decode($request->getContent(), true);
+
+
+//     if ($data === null || !array_key_exists('selectedBirds', $data)) {
+//         return new JsonResponse(['error' => 'Los datos recibidos no son válidos.']);
+//     }
+
+
+//     $selectedBirdsData = $data['selectedBirds'];
+
+
+//     if (empty($selectedBirdsData)) {
+//         return new JsonResponse(['error' => 'No se han recibido los datos esperados.']);
+//     }
+
+
+//     // Puedes utilizar $selectedBirds para realizar operaciones adicionales aquí
+
+
+//     $response = new JsonResponse(['selectedBirdsData' => $selectedBirdsData]);
+//     $response->headers->set('Access-Control-Allow-Origin', '*');
+//     $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     $response->headers->set('Access-Control-Max-Age', '3600');
+//     return $response;
+// }
+
+
+
+
+// }
